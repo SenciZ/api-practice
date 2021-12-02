a9e05ce4b1316a15569ac133276519ce7f41006d
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-01 17:07:00 -0700

    refactor code

--- swapi/main.js
+++ swapi/main.js
@@ -1,31 +1,24 @@
-const getResidentsBtn = document.querySelector('button');
+const getResidentsBtn = document.querySelector("button");
 
-
-const residentsCallback = (obj)=>{
-    console.log(obj)
-    console.log(obj.results[0].residents)
-    obj.results[0].residents.forEach(resident => {
-        console.log(resident)
-        axios
-        .get(`${resident}`).then((res) => {residentThenCallback(res.data)})
-    });
- }
-
-
- const residentThenCallback = (data)=>{
-    const nameH2 = document.createElement('h2')
-    nameH2.textContent = `${data.name}`
-    const htmlDocu = document.querySelector("html")
-    htmlDocu.appendChild(nameH2)
- }
-
-
-const clicked = ()=>{
-    // alert("clicked")
+const residentsCallback = (res) => {
+  res.data.results[0].residents.forEach((resident) => {
     axios
-        .get('https://swapi.dev/api/planets/?search=alderaan')
-        .then((res)=> residentsCallback(res.data))
-        .catch
-}
+    .get(`${resident}`)
+    .then(residentThenCallback);
+  });
+};
 
-getResidentsBtn.addEventListener("click", clicked)
+const residentThenCallback = (res) => {
+  const nameH2 = document.createElement("h2");
+  nameH2.textContent = `${res.data.name}`;
+  const htmlDocu = document.querySelector("html");
+  htmlDocu.appendChild(nameH2);
+};
+
+const clicked = () => {
+  axios
+    .get("https://swapi.dev/api/planets/?search=alderaan")
+    .then(residentsCallback);
+};
+
+getResidentsBtn.addEventListener("click", clicked);
